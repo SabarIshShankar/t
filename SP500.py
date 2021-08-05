f8f934b45f13ef28621f475c451c8133cd21a6eb
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-05 19:24:32 +0530

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -7,29 +7,70 @@ df = pd.read_excel('https://github.com/kdboller/pythonsp500-plotly-dash/blob/mas
 df.head(10)
 
 df.describe()
-
 df.info()
+start_sp = datetime.datetime(2010,1,1)
+end_sp = datetime.datetime(2021,1,1)
 
-start = datetime.datetime(2010,1,1)
-end = datetime.datetime(2018,7,20)
+end_of_last_year = datetime.datetime(2018, 12 ,2)
 
-end_of_last_year = datetime.datetime(2017, 12,29)
-
-stock_start = datetime.datetime(2010, 1, 1)
-stocks_end = datetime.datetime(2018, 7, 13)
+stocks_start = datetime.datetime(2010,1,1)
+stocks_end = datetime.datetime(2021,1,1) 
 
 from pandas_datareader import data as pdr
 import yfinance as yf
-
-from pandas_datareader import data as pdr
 yf.pdr_override()
-sp500 = pdr.get_data_yahoo('^GSPC', start, end)                        
+sp500 = pdr.get_data_yahoo('^GSPC',start_sp,end_sp)
 sp500.head()
 
+
+# In[11]:
+
+
 sp500.tail()
+
+
+# In[12]:
+
 
 sp_500_adj_close = sp500[['Adj Close']].reset_index()
 sp_500_adj_close.tail()
 
+sp_500_adj_close.tail()
+
 tickers = df['Ticker'].unique()
-print(tickers)
+tickers
+
+def get(tickers, startdate, enddate):
+    def data(ticker):
+        return (pdr.get_data_yahoo(ticker, start=startdate, end=enddate))
+    datas = map(data, tickers)
+    return(pd.concat(datas, keys=tickers, names=['Ticker','Date']))
+
+all_data = get(tickers, stocks_start, stocks_end)
+
+all_data.tail()
+
+all_data.head()
+
+adj_close=all_data[['Adj Close']].reset_index()
+adj_close.head()
+
+adj_close.plot()
+
+adj_close['Ticker'].unique()
+
+adj_close_start = adj_close[adj_close['Date']==end_of_last_year]
+adj_close.head()
+
+adj_close_latest = adj_close[adj_close['Date']==stocks_end]
+adj_close_latest.tail()
+
+merge = pd.merge(df, adj_close_latest, on='Ticker')
+merge.head(10)
+
+merge['ticker return'] = merge['Adj Close']/merge['Unit Cost']-1
+merge.head()
+
+merged_sp = pd.merge(merge, sp_500_adj_close, left_on='Acquisition Date', right_on='Date')
+merged_sp.head()
+
